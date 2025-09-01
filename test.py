7a75d76740a1a494690939297c72f51cb7b4af39
Homely Assist <homeconnecteasy@gmail.com>
2025-09-01 12:00:32 +0530

    Day: 1 (if else)

---
+++ test.py
@@ -0,0 +1,28 @@
+# input_data = int(input("Enter number"))
+#
+# if input_data % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# Multi Condition
+
+grade = float(input("Enter Grade: "))
+# 9.5
+# if grade >= 9:
+#     print("A")
+# elif grade >= 8:
+#     print("B")
+# elif grade >= 6:
+#     print("C")
+# else:
+#     print("F")
+
+# if grade >= 6:
+#     print("C")
+# elif grade >= 8:
+#     print("B")
+# elif grade >= 9:
+#     print("A")
+# else:
+#     print("F")
