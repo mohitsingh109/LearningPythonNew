d8458d4b130c97238564bc227b04e64dfefeb756
Homely Assist <homeconnecteasy@gmail.com>
2025-09-01 12:25:02 +0530

    Day: 1 (loop)

---
+++ loops.py
@@ -0,0 +1,20 @@
+# For loop: [Data traversal, array, set, dict, tuple]
+
+# 10 X 1 = 10
+# 10 X 2 =
+# 10 X 3 =
+# ...
+# 10 X 10 =
+
+# for i in range(1, 11): i = 2
+#     # print(f"10 x {i} = {10 * i}")
+#     data = 10 * i # data = 10 * 2 = 20
+#     print("10 X " + str(i) + " = " + str(data))
+
+i = 1
+
+while i < 11: # 2 < 11
+    cal = 10 * i
+    print("10 X " + str(i) + " = " + str(cal)) # 10 X 1 = 10
+    i = i + 1 # i = 3
+
